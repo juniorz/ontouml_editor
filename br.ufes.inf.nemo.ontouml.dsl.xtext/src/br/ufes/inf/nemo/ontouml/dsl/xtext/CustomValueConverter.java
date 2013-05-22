eeb5526c2efd1b31ebec38f460661a251b14275c
Reinaldo Junior <juniorz@gmail.com>
2013-05-22 02:01:20 -0300

    Value converter for Cardinality

--- br.ufes.inf.nemo.ontouml.dsl.xtext/src/br/ufes/inf/nemo/ontouml/dsl/xtext/CustomValueConverter.java
+++ br.ufes.inf.nemo.ontouml.dsl.xtext/src/br/ufes/inf/nemo/ontouml/dsl/xtext/CustomValueConverter.java
@@ -7,6 +7,9 @@ import org.eclipse.xtext.conversion.ValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.parsetree.AbstractNode;
 
+import OntoDSL.Cardinality;
+import OntoDSL.OntoDSLFactory;
+
 //import com.google.inject.Inject;
 //import br.ufes.inf.nemo.ontouml.refontouml.
 
@@ -24,6 +27,56 @@ public class CustomValueConverter extends DefaultTerminalConverters {
 	        }
 	    };
 	}
+	
+
+	@ValueConverter(rule = "CardinalityLiteral")
+	public IValueConverter<Cardinality> CardinalityLiteral(){
+		return new IValueConverter<Cardinality>(){
+
+			public Cardinality toValue(String string, AbstractNode node)
+					throws ValueConverterException {
+
+				Cardinality card = OntoDSLFactory.eINSTANCE.createCardinality();
+				if(string == "*"){
+					card.setLower(0);
+					card.setUpper(-1);
+				}else if(string == "+"){
+					card.setLower(1);
+					card.setUpper(-1);					
+				}else if (string == "?"){
+					card.setLower(0);
+					card.setUpper(1);
+				}
+				else {
+					throw new ValueConverterException("Couldn't convert '" + string + "' to Cardinality", node, null);
+				}
+
+				return card;
+			}
+
+			public String toString(Cardinality value)
+					throws ValueConverterException {
+
+				String token;
+				int lower = value.getLower();
+				int upper = value.getUpper();
+
+				if(lower == 0 && upper == 1){
+					token = "?";
+				} else if(lower == 0 && upper == -1){
+					token = "*";
+				} else if(lower == 1 && upper == -1 ){
+					token = "+";
+				}else {
+					throw new ValueConverterException("Couldn't convert to CardinalityLiteral", null, null);
+				}
+
+				return token;
+			}
+			
+		};
+	}
+
 	
 /*
 	@ValueConverter(rule = "PropertyBound")
